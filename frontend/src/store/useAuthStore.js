aa1e1153e040ef1aa40e5c99f148d6f94d743283
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-09 19:12:14 +0500

    Services

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,9 +1,30 @@
 import { create } from 'zustand';
+import { axiosInstance } from '../services/api.js';
 
 export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
-  isCheckingAuth: true
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get('/users/check');
+      set({
+        authUser: response.data.user
+      });
+    } catch (error) {
+      console.error('Error checking authentication:', error);
+      set({
+        authUser: null
+      });
+    } finally {
+      set({
+        isCheckingAuth: false
+      });
+    }
+  },
+
+  signup: async (userData) => {}
 }));
