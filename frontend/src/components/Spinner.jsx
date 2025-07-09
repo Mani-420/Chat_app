41e587eed1d7707c7053818817053f43be16ebf5
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-09 19:12:30 +0500

    Components and Pages

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="flex items-center justify-center min-h-[100px]">
+    <div className="w-15 h-15 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+  </div>
+);
+
+export default Spinner;
